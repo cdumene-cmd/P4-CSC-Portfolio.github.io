dbe0d89449b080e0cf5ac3161e5174c457522bc6
cdumene-cmd <clarence.dumene@gmail.com>
2026-04-09 20:57:59 -0400

    Add files via upload

---
+++ Project 3/app.js
@@ -0,0 +1,43 @@
+var main = function() {
+    var state;
+  $('#play').click(function(){
+    $('#message').text("Playing track");
+    $('#player').trigger("play");
+   });
+
+  $("#pause").click(function() {
+    audio.pause();
+    $("#message").text("Track paused");
+  });
+  $("#mute").click(function() {
+    audio.muted = true;
+    $("#message").text("Track muted");
+  });
+
+  $("#unmute").click(function() {
+    audio.muted = false;
+    $("#message").text("Track unmuted");
+  });
+  $("#stop").click(function() {
+    audio.pause();
+    audio.currentTime = 0;
+    $("#message").text("Track stopped");
+  });
+  $("#volUp").click(function() {
+    if (audio.volume < 1) {
+        audio.volume += 0.1;
+        if (audio.volume > 1) audio.volume = 1;
+    }
+    $("#message").text("Volume: " + Math.round(audio.volume * 100) + "%");
+  });
+
+  $("#volDown").click(function() {
+    if (audio.volume > 0) {
+        audio.volume -= 0.1;
+        if (audio.volume < 0) audio.volume = 0;
+    }
+    $("#message").text("Volume: " + Math.round(audio.volume * 100) + "%");
+  });
+}
+
+$(document).ready(main);
